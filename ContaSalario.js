1b2f33f15d4409cd27cd3747bfc5c548fdd6ff00
alanz <alanzito006@gmail.com>
2025-10-07 23:48:32 -0300

    docs: criação do arquivo JS da classe ContaSalario

---
+++ ContaSalario.js
@@ -0,0 +1,14 @@
+// Louvado seja o Senhor
+
+import { Conta } from "./Conta.js";
+
+export class ContaSalario extends Conta{
+    constructor(cliente){
+        super(0, cliente, 1001);
+    }
+
+    sacar(valor){
+        const taxa = 1.01;
+        return this._sacar(valor, taxa);
+    }
+}
