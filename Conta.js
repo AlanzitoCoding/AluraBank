ebeb7d8e92026245f1ec6a2d996884a20536716f
alanz <alanzito006@gmail.com>
2025-10-07 23:49:09 -0300

    refactor: abstração do método sacar na classe Conta

--- Conta.js
+++ Conta.js
@@ -5,7 +5,7 @@ import { Cliente } from "./Cliente.js";
 export class Conta{
     constructor(saldoInicial, cliente, agencia){
         if(this.constructor === Conta){
-            throw new Error("Você não deve instanciar um objeto do tipo Conta!");
+            throw new Error("Você não deve instanciar um objeto do tipo Conta diretamente");
         }
 
         this._saldo = saldoInicial;
@@ -27,9 +27,9 @@ export class Conta{
         return this._saldo;
     }
     
+    // Método Abstrato
     sacar(valor){
-        let taxa = 1;
-        return this._sacar(valor, taxa);
+        throw new Error("O método sacar é abstrato");
     }
     
     _sacar(valor, taxa){
